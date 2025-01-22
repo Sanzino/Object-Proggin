338ac867b868f764745005c16cbf0346b56ed22d
Shaffi Sanzan <Shaffijsanza@gmail.com>
2025-01-22 21:27:07 +0100

    Initial commit for Moduler
    git rm --cached .DS_Store

---
+++ Modul01/src/MadLibs.java
@@ -0,0 +1,55 @@
+public class MadLibs {
+    /*
+This program generates Mad-libbed story.
+Author: Shaffi Sanza
+Date:13.01.2025
+*/
+    public static void main(String[] args) {
+
+        // Declaring the main character
+
+        String name1 ="Shaffi Sanza";
+        String name2 ="Maria Dasilva";
+
+
+        // Declaring the adjectives
+
+        String adjective1 = "horny";
+        String adjective2 = "good, fcking day";
+        String adjective3 = "hype";
+
+        //verb declaration
+        String verb1 = "shoot guns";
+
+        //declaration of nouns
+        String noun1 ="bitches";
+        String noun2 ="niggas fly";
+        String noun3 ="gunna spitting crack";
+        String noun4 ="niggas";
+        String noun5 ="white ppl";
+        String noun6 ="Niggas with attitude";
+
+        //declaration of place
+        String place1="LA";
+
+        //Declaration of number
+        int number = 2025;
+
+        //The template for the story
+
+        String story = "This morning "+name1+" woke up feeling "+adjective1+"." +
+                " 'It is going to be a "+adjective2+" day!' Outside, a bunch of "
+                +noun1+"s were protesting to keep "+noun2+" in stores. They began " +
+                "to "+verb1+" to the rhythm of the "+noun3+", which made all the" +
+                " "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+"," +
+                " who flew "+name1+" to "+place1+" and dropped "+name1+" in a" +
+                " puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+"," +
+                " in a world where "+noun6+"s ruled the world.";
+        System.out.println(story);
+
+
+
+
+    }
+
+}
