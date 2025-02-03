c4999eeda4be6969dabb7818c524af0d78f12345
Shaffi Sanzan <Shaffijsanza@gmail.com>
2025-02-03 13:33:15 +0100

    Oppgave 1 modul4

---
+++ Modul04/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Book b= new Book();
+        b.setAuthor("Shafi The goat");
+        b.setTitle("How to be goated");
+        b.setNumberOfPages(1);
+        b.setGenre(Genre.CLASSIC);
+
+        b.printMe();
+    }
+}
