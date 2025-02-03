e411ff865e69837e1bff7db1b246ce11d8fd8fcc
Shaffi Sanzan <Shaffijsanza@gmail.com>
2025-02-03 13:33:15 +0100

    Oppgave 1 modul4

--- Modul04/Book.java
+++ Modul04/Book.java
@@ -1,8 +1,47 @@
 public class Book {
-
     private String title;
+
     private String author;
+
     private int numberOfPages;
 
-    public void
+    private Genre genre;
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public void printMe(){
+        System.out.println(" Title:" + title + " author:" + author + " pages" + numberOfPages + " Genre:" + genre);
+    }
+
 }
